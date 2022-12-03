f5bc88cf62c8c28f78d02fc9d6e35cd38d5cdb44
Gustav Rasmussen <gcr84@hotmail.com>
2022-12-03 12:52:01 +0100

    chore: add day 3

---
+++ 2022/day_3/rucksack_reorganization.py
@@ -0,0 +1,48 @@
+# from pprint import pprint as pp
+import string
+
+datafiles = ["input.txt", "sample.txt"]
+datafile = datafiles[0]
+
+with open(datafile, "r") as rf:
+    lines = rf.readlines()
+
+lines = [line.removesuffix("\n") for line in lines]
+
+
+def part_1():
+    common_characters = ""
+    for line in lines:
+        # line = line.removesuffix("\n")
+        assert len(line) % 2 == 0
+        compartment_1 = line[: round(len(line) / 2)]
+        compartment_2 = line[round(len(line) / 2) :]
+        common_characters += "".join(set(compartment_1).intersection(compartment_2))
+    return common_characters
+
+
+priorities = {letter: i + 1 for i, letter in enumerate(string.ascii_letters)}
+
+# pp(lines)
+# pp(priorities)
+
+# part 1
+# common_characters = part_1()
+# print(sum([priorities[letter] for letter in common_characters]))
+
+# part 2
+def divide_elves(lines: list, n: int):
+    for i in range(0, len(lines), n):
+        yield lines[i : i + n]
+
+
+elf_groups = list(divide_elves(lines, 3))
+
+common_characters = ""
+for elf_group in elf_groups:
+    # print(elf_group)
+    common_characters += "".join(
+        set(elf_group[0]).intersection(elf_group[1]).intersection(elf_group[2])
+    )
+# print(common_characters)
+print(sum([priorities[letter] for letter in common_characters]))
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Advent calendar of programming puzzles
 
 ## Progress - AoC 2022
 
-![advent_of_code_status_2022_12_02](2022/advent_of_code_status_2022_12_02.jpg)
+![advent_of_code_status_2022_12_03](2022/advent_of_code_status_2022_12_03.jpg)
