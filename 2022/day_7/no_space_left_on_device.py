102491d78332e7e048fe9a83fccf6f9b686179e9
Gustav Rasmussen <gcr84@hotmail.com>
2022-12-07 21:37:26 +0100

    chore: add day 7 WIP

---
+++ 2022/day_7/no_space_left_on_device.py
@@ -0,0 +1,95 @@
+import re
+
+from treelib import Tree
+
+datafiles = ["input.txt", "sample.txt"]
+datafile = datafiles[0]
+
+with open(datafile, "r") as rf:
+    lines = rf.readlines()
+
+lines = [line.removesuffix("\n") for line in lines]
+
+cd_dir_regex = re.compile(r"^\$\scd\s(.*)")
+dir_regex = re.compile(r"^dir\s(.*)")
+file_regex = re.compile(r"^(\d*)\s(.*)")
+
+
+class File:
+    def __init__(self, size):
+        self.size = size
+
+
+def get_tree():
+    dir_names = set()
+    tree = Tree()
+    tree.create_node("/", "/", data=File(0))  # root tree node
+    current_dir = "/"
+
+    for line in lines:
+        is_changing_dir = re.search(cd_dir_regex, line)
+        dir_content = re.search(dir_regex, line)
+        file_content = re.search(file_regex, line)
+
+        if is_changing_dir:
+            current_dir = is_changing_dir.group(1)
+            if current_dir == "..":
+                continue
+
+            # while current_dir in dir_names:
+            #     current_dir += "_$"
+            # dir_names.add(current_dir)
+
+        if dir_content:
+            dir_name = dir_content.group(1)
+            while dir_name in dir_names:
+                dir_name += "_$"
+            dir_names.add(dir_name)
+
+            tree.create_node(
+                dir_name,
+                dir_name,
+                parent=current_dir,
+                data=File(0),
+            )
+
+        if file_content:
+            orig_file_size = tree.get_node(current_dir).data.size
+            extra_file_size = int(file_content.group(1))
+            file_size = orig_file_size + extra_file_size
+            # if file_size == 0:
+            #     print("filesize error:\n")
+            #     print(tree.get_node(current_dir))
+            #     break
+            tree.update_node(nid=current_dir, data=File(file_size))
+
+    return tree
+
+
+tree = get_tree()
+tree.show()
+
+
+def update_tree():
+    for node in tree.all_nodes()[::-1]:
+        if node.is_leaf():
+            continue
+        for child in tree.children(node.tag):
+            data = File(node.data.size + child.data.size)
+            tree.update_node(node.tag, data=data)
+
+
+update_tree()
+tree.show(data_property="size")
+
+
+def sum_small_nodes():
+    small_nodes = []
+    for node in tree.all_nodes():
+        if node.data.size <= 100_000:
+            small_nodes.append(node.data.size)
+    return sum(small_nodes)
+
+
+small_nodes_sum = sum_small_nodes()
+print(small_nodes_sum)
