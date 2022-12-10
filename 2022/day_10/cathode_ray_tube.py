e0bbb45eb62ed8329e450cc07a54098b7a32f3c3
Gustav Rasmussen <gcr84@hotmail.com>
2022-12-11 00:34:16 +0100

    chore: update day 10 WIP

--- 2022/day_10/cathode_ray_tube.py
+++ 2022/day_10/cathode_ray_tube.py
@@ -1,21 +1,25 @@
-datafiles = ["input.txt", "sample.txt"]
+datafiles = ["input.txt", "sample.txt", "sample_2.txt"]
 datafile = datafiles[1]
 with open(datafile, "r") as rf:
     lines = rf.readlines()
 lines = [line.removesuffix("\n") for line in lines]
 
 x = 1
-cycle_no = 0
+instruction_queue = []
 
 
 def get_signal_strength(x, cycle_no):
     return x * cycle_no
 
 
-for line in lines:
+for idx, line in enumerate(lines):
+    cycle_no = idx + 1
     if line == "noop":
-        cycle_no += 1
-    else:
-        instruction, v = line.split()
-        print(instruction)
-        print(v)
+        # cycle_no += 1
+        continue
+    _, v = line.split()
+    v = int(v)
+    print(v)
+    instruction_queue.append(v)
+    x += instruction_queue.pop(0)
+    print(f"{x = }")
