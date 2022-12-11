a3a463d68fec0f140f65a2a2fd349eb43a169e00
Gustav Rasmussen <gcr84@hotmail.com>
2022-12-11 13:26:27 +0100

    chore: update day 10 WIP

--- 2022/day_10/cathode_ray_tube.py
+++ 2022/day_10/cathode_ray_tube.py
@@ -1,5 +1,5 @@
 datafiles = ["input.txt", "sample.txt", "sample_2.txt"]
-datafile = datafiles[1]
+datafile = datafiles[0]
 with open(datafile, "r") as rf:
     lines = rf.readlines()
 lines = [line.removesuffix("\n") for line in lines]
@@ -19,22 +19,37 @@ for idx, line in enumerate(lines):
     print(f"{x = }, {line = }\n")
 
     if instruction_queue:
-        x += instruction_queue.pop(0)
+        for k, v in instruction_queue[0].items():
+            instruction_queue[0][k] -= 1
+        if list(instruction_queue[0].values())[0] == 0:
+            x += int(list(instruction_queue[0].keys())[0])  # instruction_queue.pop(0)
+            instruction_queue.pop(0)
 
     if line == "noop":
+        print(f"end of cycle: {cycle_no}")
+        print(f"{x = }")
         continue
+
+    _, v = line.split()
+    cycles_to_wait = 1
+    instruction_queue.append({v: cycles_to_wait})
+    # cycles_to_wait -= 1
 
     print(f"end of cycle: {cycle_no}")
     print(f"{x = }")
 
-    _, v = line.split()
-    instruction_queue.append(int(v))
-
-
 while instruction_queue:
+
     cycle_no += 1
     print(f"start of cycle: {cycle_no}")
     print(f"{x = }")
-    x += instruction_queue.pop(0)
+
+    for k, v in instruction_queue[0].items():
+        instruction_queue[0][k] -= 1
+
+        if list(instruction_queue[0].values())[0] == 0:
+            x += int(list(instruction_queue[0].keys())[0])
+            instruction_queue.pop(0)
+
     print(f"end of cycle: {cycle_no}")
     print(f"{x = }")
