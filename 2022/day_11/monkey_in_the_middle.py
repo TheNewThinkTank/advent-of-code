d94dc4fb00cd29d66520aa6610f94e4b12bf89e9
Gustav Rasmussen <gcr84@hotmail.com>
2022-12-13 22:24:02 +0100

    chore: update day 11

--- 2022/day_11/monkey_in_the_middle.py
+++ 2022/day_11/monkey_in_the_middle.py
@@ -7,18 +7,25 @@ from pprint import pprint as pp
 class Monkey:
     id: int
     starting_items: list
-    num: int
-    worry_level: int
+    test_divisible_by: int
     operation: str
+    next_monkey_on_true: int
+    next_monkey_on_false: int
 
     def update_worry_level(self):
-        return eval(self.operation)
+        old = self.starting_items[0]
+        res = eval(self.operation)
+        self.starting_items[0] = res
+        return res
 
     def test(self) -> bool:
-        return self.worry_level % self.num == 0
+        return self.starting_items[0] % self.test_divisible_by == 0
 
+    def remove_item(self):
+        del self.starting_items[0]
 
-# worry_level: int
+    def receive_item(self, item):
+        self.starting_items.append(item)
 
 
 def get_raw_monkeys():
@@ -35,14 +42,12 @@ def get_raw_monkeys():
 
 
 raw_monkeys = get_raw_monkeys()
-# pp(raw_monkeys)
+clean_monkeys = []
 
-monkeys = []
 for monkey in raw_monkeys:
-
     monkey_id = int(monkey[0].split()[-1])
     starting_items = [int(x) for x in monkey[1].split(":")[-1].strip().split(", ")]
-    operation = monkey[2].split(":")[-1].strip()
+    operation = monkey[2].split(":")[-1].strip().replace("new = ", "")
     test_divisible_by = int(monkey[3].split(":")[-1].strip().split()[-1])
     next_monkey_on_true = int(monkey[4].split(":")[-1].strip().split()[-1])
     next_monkey_on_false = int(monkey[5].split(":")[-1].strip().split()[-1])
@@ -55,6 +60,21 @@ for monkey in raw_monkeys:
         "next_monkey_on_true": next_monkey_on_true,
         "next_monkey_on_false": next_monkey_on_false,
     }
-    monkeys.append(clean_monkey)
+    clean_monkeys.append(clean_monkey)
 
-pp(monkeys)
+# pp(clean_monkeys)
+monkeys = [Monkey(**clean_monkey) for clean_monkey in clean_monkeys]
+# pp(monkeys)
+
+first_monkey = monkeys[0]
+
+for starting_item in first_monkey.starting_items:
+    print(starting_item)
+
+    updated_worry_level = first_monkey.update_worry_level()
+
+    print(starting_item, updated_worry_level)
+
+    # first_monkey.test()
+
+    # next monkey receives
