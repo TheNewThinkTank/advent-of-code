94619e9585e7e78914a8a48ac45dc574a2070108
Gustav Rasmussen <gcr84@hotmail.com>
2023-12-17 16:19:03 +0100

    chore: progress day_8

--- 2023/day_8/haunted_wasteland.py
+++ 2023/day_8/haunted_wasteland.py
@@ -11,25 +11,35 @@ datafiles = [
 
 datafile = datafiles[1]
 
-with open(datafile, "r") as rf:
-    lines = rf.readlines()
-lines = [line.removesuffix("\n") for line in lines]
-lines = [line for line in lines if line]
-left_right_instructions = lines[0]
-elements = lines[1:]
 
-# pp(left_right_instructions)
-# for element in elements:
-#     eval(element)
-# pp(elements)
+def get_instructions_and_nodes():
+    with open(datafile, "r") as rf:
+        lines = rf.readlines()
+    lines = [line.removesuffix("\n") for line in lines]
+    lines = [line for line in lines if line]
+    left_right_instructions = lines[0]
+    elements = lines[1:]
+    nodes_lr = {}
+    for element in elements:
+        first = element.partition(" = ")
+        nodes_lr[first[0]] = first[2].strip("()").split(", ")
+    return left_right_instructions, nodes_lr
 
-first = elements[0].partition(" = ")
 
-first_node = first[0]
+left_right_instructions, nodes_lr = get_instructions_and_nodes()
+
+ic(left_right_instructions)
+ic(nodes_lr)
+
+index_map = {"R": 1, "L": 0}
+
+first_node = list(nodes_lr.keys())[0]
+first_instruction = left_right_instructions[0]
+
 ic(first_node)
+ic(first_instruction)
 
-first_lr = first[2].strip("()").split(", ")
-ic(first_lr)
+ic(nodes_lr[first_node][index_map[first_instruction]])
 
 # for instruction in left_right_instructions:
 #     ic(instruction)
