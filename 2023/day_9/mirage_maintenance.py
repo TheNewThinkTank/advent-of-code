2fdeb83a7a0b787579aae188a072e61f416ed977
Gustav Rasmussen <gcr84@hotmail.com>
2023-12-22 20:20:24 +0100

    feat: complete day_9

--- 2023/day_9/mirage_maintenance.py
+++ 2023/day_9/mirage_maintenance.py
@@ -56,10 +56,30 @@ def update_all_sequences(all_sequences: list) -> None:
 
 
 all_sequences = make_all_sequences()
-update_all_sequences(all_sequences)
 
+# part 1
+# update_all_sequences(all_sequences)
+# extrapolated_sum = sum([
+#     l[0][-1]
+#     for l in tqdm(all_sequences)
+# ])
+
+# part 2
+
+
+def update_all_sequences_part_2(all_sequences: list) -> None:
+    for seq in all_sequences:
+        seq = list(reversed(seq))
+        for idx, s in enumerate(seq):
+            if idx == 0:
+                s.append(0)
+            else:
+                s.insert(0, s[0] + seq[idx - 1][0] * -1)
+
+
+update_all_sequences_part_2(all_sequences)
 extrapolated_sum = sum([
-    l[0][-1]
+    l[0][0]
     for l in tqdm(all_sequences)
 ])
 ic(extrapolated_sum)
