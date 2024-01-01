07717bfb96632904cae141f93092d8ce8f8eb5b5
Gustav Rasmussen <gcr84@hotmail.com>
2024-01-01 20:30:44 +0100

    chore: complete 2015 day 3

--- 2015/day_3/perfectly_spherical_houses_in_a_vacuum.py
+++ 2015/day_3/perfectly_spherical_houses_in_a_vacuum.py
@@ -10,7 +10,6 @@ examples:
 ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 """
 
-import numpy as np
 from icecream import ic
 
 
@@ -26,61 +25,49 @@ def get_data():
     return data
 
 
-directions = "^>v<"  # get_data()
+directions = get_data()  # "^>v<"
 # ic(directions)
 
-houses = np.ones((1, 1))
-current_loc = [0, 0]
 
-# ic(houses)
-shape = np.shape(houses)
-ic(np.shape(houses))
-
-
-def update_current_loc(dir):
+def get_new_loc(dir, current_loc):
+    new_loc = current_loc
     if dir == ">":
-        current_loc[1] += 1
+        new_loc = (current_loc[0], current_loc[1] + 1)
     if dir == "<":
-        current_loc[1] -= 1
+        new_loc = (current_loc[0], current_loc[1] - 1)
     if dir == "^":
-        current_loc[0] -= 1
+        new_loc = (current_loc[0] - 1, current_loc[1])
     if dir == "v":
-        current_loc[0] += 1
-    return current_loc
+        new_loc = (current_loc[0] + 1, current_loc[1])
+    return new_loc
 
 
+current_loc = (0, 0)
+locations = []
+locations.append(current_loc)
+
+# part 1
 # for dir in directions:
-dir = directions[0]
-current_loc = update_current_loc(dir)
-ic(current_loc)
+#     new_loc = get_new_loc(dir, current_loc)
+#     current_loc = new_loc
+#     locations.append(new_loc)
+# total_houses = len(set(locations))
+# ic(total_houses)
 
+# part 2
+santa_dir = directions[::2]
+robo_dir = directions[1::2]
 
-def resize_houses(houses, current_loc):
-    # Get the current shape of the houses array
-    current_shape = houses.shape
+for dir in santa_dir:
+    new_loc = get_new_loc(dir, current_loc)
+    current_loc = new_loc
+    locations.append(new_loc)
 
-    # Check if the current_loc id is within the current shape
-    if current_loc[0] < current_shape[0] and current_loc[1] < current_shape[1]:
-        print("Current location is within the current shape.")
-    else:
-        # Calculate the new shape that includes the current_loc id
-        new_shape = (max(current_loc[0]+1, current_shape[0]), max(current_loc[1]+1, current_shape[1]))
+current_loc = (0, 0)
+for dir in robo_dir:
+    new_loc = get_new_loc(dir, current_loc)
+    current_loc = new_loc
+    locations.append(new_loc)
 
-        # Create a new array with the new shape and copy the values from the original array
-        new_houses = np.zeros(new_shape)
-        new_houses[:current_shape[0], :current_shape[1]] = houses
-
-        # Update the houses array with the resized array
-        houses = new_houses
-        print("Houses array resized to contain the current location.")
-
-    return houses
-
-
-houses = resize_houses(houses, current_loc)
-ic(houses)
-
-# houses = np.insert(houses, current_loc, 1)
-# houses.resize(new_shape)
-# ic(current_loc)
-# ic(houses)
+total_houses = len(set(locations))
+ic(total_houses)
