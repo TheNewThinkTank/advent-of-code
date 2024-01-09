6d770f34cf088fc17cd951efdb4db7531f0026cb
Gustav Rasmussen <gcr84@hotmail.com>
2024-01-09 22:33:16 +0100

    chore: complete day 6

--- 2015/day_6/fire_hazard.py
+++ 2015/day_6/fire_hazard.py
@@ -27,14 +27,12 @@ def get_action_and_coords(test):
 
 
 def get_new_value(action, value) -> int:
-    # def toggle(value) -> int:
-    #     return 0 if value else 1
     action_map = {
         "turn on": 1,
         "turn off": 0,
         "toggle": np.logical_not(value).astype(
             int
-        ),  # 0 if value else 1  # toggle(value),
+        ),
     }
     return action_map[action]
 
@@ -59,14 +57,24 @@ with open("input.txt", "r") as rf:
     tests = [test.strip("\n") for test in tests]
 # ic(tests)
 grid = np.zeros((1_000, 1_000))  # , dtype=np.bool_)
+action_map = {
+    "turn on": 1,
+    "turn off": -1,
+    "toggle": 2
+}
 for test in tests:
     action, start_coords, end_coords = get_action_and_coords(test)
     # ic(action)
     # ic(start_coords)
     # ic(end_coords)
-    update_grid(action, start_coords, end_coords)
-ic(grid)
 
-# lights_on = sum(grid)
+    # part 1
+    # update_grid(action, start_coords, end_coords)
+
+    # part 2
+    grid[start_coords[0] : end_coords[0] + 1, start_coords[1] : end_coords[1] + 1] += action_map[action]
+    grid[grid < 0] = 0
+
+ic(grid)
 lights_on = int(np.sum(grid))
 ic(lights_on)
